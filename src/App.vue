cc5a775386eddb69db18ab27059740e49b351f9f
Jun Jie Lin <junjie@vitrion.nl>
2025-02-24 16:40:51 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
+<script setup lang="ts">
+import 'primeicons/primeicons.css'
+</script>
 <template>
-  <div class="min-h-screen bg-gray-100 text-gray-900">
-    <nav class="bg-gray-600 p-4 shadow-md">
+  <div class="min-h-screen text-gray-200">
+    <!-- Navbar -->
+    <nav class="bg-gray-800 p-4 shadow-md">
       <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-white text-2xl font-bold">Meme Generator</h1>
+        <h1 class="text-white text-3xl font-bold flex items-center">
+          <i class="pi pi-image mr-2"></i> Meme Generator
+        </h1>
 
         <div class="flex space-x-4">
-          <RouterLink to="/" class="nav-link">🏠 Home</RouterLink>
-          <RouterLink to="/memes" class="nav-link">🖼 Bekijk Memes</RouterLink>
-          <RouterLink to="/create" class="nav-link">🎨 Maak een Meme</RouterLink>
+          <RouterLink to="/" class="nav-link"><i class="pi pi-home"></i> Home</RouterLink>
+          <RouterLink to="/memes" class="nav-link"
+            ><i class="pi pi-images"></i> Bekijk Memes</RouterLink
+          >
+          <RouterLink to="/create" class="nav-link"
+            ><i class="pi pi-palette"></i> Maak een Meme</RouterLink
+          >
         </div>
       </div>
     </nav>
 
-    <div class="container mx-auto mt-6 p-6 bg-white shadow-lg rounded-md">
+    <!-- Main Content -->
+    <div class="container mx-auto mt-6 p-6 sshadow-lg rounded-md">
       <router-view></router-view>
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
